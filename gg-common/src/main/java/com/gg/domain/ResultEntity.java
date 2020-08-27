e0b542d73e35c38e8ad0a450e37ebe74fb3db3fd
gengdz <727843952@qq.com>
2020-08-27 19:27:36 +0800

    跨域处理

--- gg-common/src/main/java/com/gg/domain/ResultEntity.java
+++ gg-common/src/main/java/com/gg/domain/ResultEntity.java
@@ -51,10 +51,6 @@ public class ResultEntity implements Serializable {
         return new ResultEntity(CODE_SUCCESS,msg,data);
     }
 
-    public static ResultEntity success(String code, String msg) {
-        return new ResultEntity(code,msg,null);
-    }
-
     public static ResultEntity success(String msg) {
         return new ResultEntity(CODE_SUCCESS,msg,null);
     }
@@ -69,10 +65,6 @@ public class ResultEntity implements Serializable {
 
     public static ResultEntity fail(String msg, Object data) {
         return new ResultEntity(CODE_FAIL,msg,data);
-    }
-
-    public static ResultEntity fail(String code, String msg) {
-        return new ResultEntity(code,msg,null);
     }
 
     public static ResultEntity fail(String msg) {
